b02a8da58f6c2bd438f5f770d3e0af1a8adbd29f
Kai <ksah466@aisbmali.org>
2021-10-18 17:14:02 +0000

    Branch: svelte

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import delay from "./utils/delay";
+
+	let apod: string = "Loading...";
+
+	onMount(async () => {
+		let res: Response = await fetch("/apod.json");
+		let resJson = await res.json();
+
+		await delay(1000);
+
+		apod = JSON.stringify(resJson, null, 2);
+	});
+</script>
+
+<main>
+	<pre>{apod}</pre>
+</main>
+
+<style>
+</style>
